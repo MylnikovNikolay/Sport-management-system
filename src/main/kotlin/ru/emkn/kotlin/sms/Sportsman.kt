55d9db0ab7e5795ad2da457a59bbeea48d24ac6c
MylnikovNikolay <mylnikovnikolay1707@gmail.com>
2021-11-24 10:59:01 +0300

    Добавил возможность расставлять столбцы в заявке на участие в любом порядке и учитывать дополнительные данные

--- src/main/kotlin/ru/emkn/kotlin/sms/Sportsman.kt
+++ src/main/kotlin/ru/emkn/kotlin/sms/Sportsman.kt
@@ -4,6 +4,19 @@ import com.github.doyaaaaaken.kotlincsv.dsl.csvReader
 import java.io.File
 
 enum class Gender{ MALE, FEMALE, UNKNOWN}
+
+private fun toGender(str: String): Gender {
+    return when(str) {
+        "муж" -> Gender.MALE
+        "муж." -> Gender.MALE
+        "мужской" -> Gender.MALE
+        "жен" -> Gender.FEMALE
+        "жен." -> Gender.FEMALE
+        "женский" -> Gender.FEMALE
+        else -> Gender.UNKNOWN
+    }
+}
+
 data class Sportsman(
     val group: String,
     val name: String,
@@ -13,8 +26,24 @@ data class Sportsman(
     val gender: Gender = Gender.UNKNOWN,  //пол
     val medExamination: String="", //данные про медосмотр
     val insurance: String="",      //страхование
-)
+) {
+    constructor(sp: Sportsman): this(sp.group, sp.name, sp.surname,
+        sp.birthYear, sp.level, sp.gender, sp.medExamination, sp.insurance)
+    constructor(row: Map<String, String>): this(getSportsmanByRow(row))
+}
 
+private fun getSportsmanByRow(row: Map<String, String>): Sportsman {
+    val name = row["Имя"] ?: throw ImportantValueIsMissing("Имя")
+    val surname = row["Фамилия"] ?: throw ImportantValueIsMissing("Фамилия")
+    val group = row["Группа"] ?: throw ImportantValueIsMissing("Группа")
+    val birthYear = row["Г.р."] ?: throw ImportantValueIsMissing("Г.р.")
+    val level = row["Разр."] ?: throw ImportantValueIsMissing("Разр.")
+    val gender = toGender(row["Пол"] ?: "")
+    val medExamination = row["Медосмотр"] ?: ""
+    val insurance = row["Страховка"] ?: ""
+
+    return Sportsman(group, name, surname, birthYear.toInt(), level, gender, medExamination, insurance)
+}
 
 class Team(
     val name: String,
@@ -26,16 +55,14 @@ class Team(
 
 private fun readTeamFromCSV(file: File): Team {
     // Ожидается формат как в sample-data:
-    // первая строка это название команды, а дальше идут описания участников
+    // первая строка это название команды, вторая это header, а дальше идут описания участников
     val rows: List< List<String> > = csvReader().readAll(file)
-    require(rows.size >= 2) { "В команде нет участников или неверный формат ввода" }
+    require(rows.size >= 3) { "В команде нет участников или неверный формат ввода" }
 
     val name = rows[0][0]
+    val header = rows[1]
     val sportsmen = rows.drop(2).map {row ->
-        require(row.size >= 4) { "Не хватает информации об участнике команды $name" }
-        check(row[3].toIntOrNull() != null) { "В столбце 'год рождения' должно быть число" }
-
-        Sportsman(group=row[0], surname=row[1], name=row[2], birthYear=row[3].toInt(), level=row[4])
+        Sportsman((header zip row).toMap())
     }
     return Team(name, sportsmen)
 }
