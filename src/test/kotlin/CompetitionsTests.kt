8017aedaa4030fbf5853769e6f7fe223370e23f0
MylnikovNikolay <mylnikovnikolay1707@gmail.com>
2021-12-07 17:15:19 +0300

    Подправил тест для fromString

--- src/test/kotlin/CompetitionsTests.kt
+++ src/test/kotlin/CompetitionsTests.kt
@@ -22,23 +22,21 @@ class TestCompetitions(comp: CompetitionsByCSV): CompetitionsByCSV(comp.name, co
 internal class CompetitionsTests {
     @Test
     fun fromStringTest() {
-        var comp = CompetitionsByCSV.fromString("Название,Дата\n" + "Первенство пятой бани,01.01.2022")
+        var comp = fromString("Название,Дата\n" + "Первенство пятой бани,01.01.2022")
         assertEquals(comp.name, "Первенство пятой бани")
         assertEquals(comp.date, "01.01.2022")
 
-        comp = CompetitionsByCSV.fromString("Дата,Название\n" + "04.01.2021,Первенство четвёртой бани")
+        comp = fromString("Дата,Название\n" + "04.01.2021,Первенство четвёртой бани")
         assertEquals(comp.name, "Первенство четвёртой бани")
         assertEquals(comp.date, "04.01.2021")
 
-        try {
-            comp = CompetitionsByCSV.fromString("abracadabra,abacaba")
-            assert(false) // Выглядит не очень, сделать потом адекватно если это возможно!!
-        } catch (_: IllegalArgumentException) { }
+        assertFailsWith<IllegalArgumentException> {
+            fromString("abracadabra,abacaba")
+        }
 
-        try {
-            comp = CompetitionsByCSV.fromString("Дота,название\n" + "Первенство пятой бани,01.01.2022")
-            assert(false)
-        } catch (_: IllegalArgumentException) { }
+        assertFailsWith<IllegalArgumentException> {
+            fromString("Дота,название\n" + "Первенство пятой бани,01.01.2022")
+        }
     }
 
 
