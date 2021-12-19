0e3c8cb2a6b0586e369d254628d2f3cbcc61b7da
GrigoriiSolnyshkin <grisolnyshkin04@gmail.com>
2021-12-19 20:23:20 +0300

    Ой, так не будет работать, забыл distance

---
+++ src/main/kotlin/ru/emkn/kotlin/sms/Distance.kt
@@ -0,0 +1,15 @@
+package ru.emkn.kotlin.sms
+
+abstract class Distance(val name: String, val controlPoints: List<ControlPoint>,){
+    init {
+        UsualLogger.log(
+            "Добавлена дистанция '$name'"
+        )
+        controlPoints.forEach {
+            UsualLogger.log(
+                "К дистанции '$name' привязан КП '${it.name}'"
+            )
+        }
+    }
+    fun findCPByName(name: String) = controlPoints.find {it.name == name}
+}
