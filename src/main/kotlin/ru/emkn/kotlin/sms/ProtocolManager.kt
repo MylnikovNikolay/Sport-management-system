1d2151bb5f55135e2ef3aa766d6e6989a88ec84a
KupuJlJl239 <kirill141103@yandex.ru>
2021-12-23 11:20:54 +0300

    Написал комментарии к новым названиям

--- src/main/kotlin/ru/emkn/kotlin/sms/ProtocolManager.kt
+++ src/main/kotlin/ru/emkn/kotlin/sms/ProtocolManager.kt
@@ -1,61 +1,69 @@
 package ru.emkn.kotlin.sms
 
-
-
 /*
 Менеджеры существуют для рутинной работы с бумажками
  */
+
 /*
-make = 
-fill = 
-create = 
-process = 
+make = генерирует новый протокол данного вида
+fill = заполняет какие-то данные из протокола
+create = создает новые объекты, указанные в названии, и заполняет их данные
+process = обрабатывает протокол (так только с teamApplication)
  */
 interface ProtocolManager {
+    //???
     fun makeResultsProtocolSimple(group: Group): String
 
+    //Генерирует протокол результатов по командам (...)
     fun makeTeamResultsProtocol(comp: Competitions): String
 
+    //Создает соревнования (...)
     fun createCompetitions(protocol: String): Competitions
 
     //Складывает результаты всех групп в единый протокол (results.csv)
     fun makeGroupResultsProtocol(comp: Competitions): String
 
-    //Обработка заявления от команды (applications)
+    //Обрабатывает заявление от команды (applications)
     fun processTeamApplication(protocol: String, comp: Competitions)
 
-    //Создание дистанций и КП из конфигурационного протокола (courses.csv)
+    //Создает дистанции и КП из конфигурационного протокола (courses.csv)
     fun createDistancesAndCPs(protocol: String, comp: Competitions)
 
-    //Создание групп из конфигурационного протокола (classes.csv)
+    //Создает группы из конфигурационного протокола (classes.csv)
     fun createGroupsAndDistances(protocol: String, comp: Competitions)
 
+    //Заполняет результаты из протокола результатов по группам (results.csv)
     fun fillResultsByGroups(protocol: String, comp: Competitions)
 
-    //Заполнение всех результатов из конфигурационного протокола (splits.csv)
+    //Заполняет результаты из протокола результатов по спортсменам (splits.csv)
     fun fillResultsBySportsmen(protocol: String, comp: Competitions)
 
+    //Заполняет результаты из протокола результатов по КП (reverseSplits)
     fun fillResultsByCPs(protocol: String, comp: Competitions)
 
+    //Заполняет время стартов (...)
     fun fillStarts(protocol: String, comp: Competitions)
 
 
 
-    //Запись стартов из стартового протокола (README.md)
+    //Заполняет старты из стартового протокола (README.md)
     fun fillStarts(protocol: String, group: Group)
 
-    //Генерация стартового протокола (README.md)
+    //Генерирует стартовый протокол (README.md)
     fun makeStartsProtocol(group: Group): String
 
-    //Генерация протокола результатов (README.md)
+    //Генерирует протокол результатов (README.md)
     fun makeResultsProtocol(group: Group): String
 
+    //Заполняет результаты группы из протокола результатов группы (README.md)
     fun fillResults(protocol: String, group: Group)
 
-    //Протокол прохождения КП (README.md)
+
+
+    //Генерирует протокол прохождения КП участниками (README.md)
     fun makeCPPassingProtocol(CP: ControlPoint): String
 
-    //Протокол прохождения дистанции (README.md)
+    //Генерирует протокол прохождения дистанции участником (README.md)
     fun makeDistancePassingProtocol(sp: CompetitionsSportsman): String
 }
 
