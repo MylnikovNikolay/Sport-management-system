f1d1a2846442f4a36904cf989750709e8282c00b
KupuJlJl239 <kirill141103@yandex.ru>
2021-12-23 14:14:07 +0300

    Научился располагать кнопки

--- src/main/kotlin/ru/emkn/kotlin/sms/gui/MainPageController.kt
+++ src/main/kotlin/ru/emkn/kotlin/sms/gui/MainPageController.kt
@@ -1,12 +1,11 @@
 package ru.emkn.kotlin.sms.gui
 import ru.emkn.kotlin.sms.*
 import androidx.compose.desktop.ui.tooling.preview.Preview
-import androidx.compose.material.Button
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Shapes
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 
@@ -26,14 +25,27 @@ class MainPageController(val comp: Competitions) {
     //Рисует все детали внутри главного окна
     @Composable @Preview fun content(){
         MaterialTheme(shapes = Shapes()) {
-            Button(onClick = ::openGroupListPage) {
+            Surface {  }
+            Button(
+                onClick = ::openGroupListPage,
+                modifier = Modifier.graphicsLayer { translationX = 0.0f; translationY = 0.0f; }
+            ) {
                 Text("Список групп")
+                Modifier.graphicsLayer {  }
             }
-            Button(onClick = ::openTeamListPage) {
+            Button(
+                onClick = ::openTeamListPage ,
+                modifier = Modifier.graphicsLayer { translationX = 0.0f; translationY = 100.0f; }
+            ) {
                 Text("Список команд")
+                //modifier = Modifier.graphicsLayer { translationX = 50.0f; translationY = 0.0f; }
             }
-            Button(onClick = ::openProtocolPage) {
+            Button(
+                onClick = ::openProtocolPage,
+                modifier = Modifier.graphicsLayer { translationX = 0.0f; translationY = 200.0f; }
+            ) {
                 Text("Работа с протоколами")
+                Modifier.graphicsLayer { translationX = 0.0f; translationY = 50.0f; }
             }
         }
     }
