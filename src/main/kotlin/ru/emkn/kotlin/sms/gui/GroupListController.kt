41030af3d38c3f79d9479bc4da5440cf5d34b5e8
KupuJlJl239 <kirill141103@yandex.ru>
2021-12-24 22:10:05 +0300

    Написал примерное содержание списка групп

--- src/main/kotlin/ru/emkn/kotlin/sms/gui/GroupListController.kt
+++ src/main/kotlin/ru/emkn/kotlin/sms/gui/GroupListController.kt
@@ -1,25 +1,46 @@
 package ru.emkn.kotlin.sms.gui
 
 import androidx.compose.desktop.ui.tooling.preview.Preview
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Shapes
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.window.Window
-import androidx.compose.ui.window.application
 import ru.emkn.kotlin.sms.Group
 
 //Окно, содержащее список групп, позволяет открывать окна групп @Composable @Preview
-class GroupListController(val groups: List<Group>) {
+class GroupListController(val groups: List<Group>, val isOpen: MutableState<Boolean>) {
 
     @Composable @Preview
     fun createWindow(){
         Window(
-            onCloseRequest = {  },
-            title = "huoybo",
+            onCloseRequest = {isOpen.value=false},
+            title = "Список групп",
         ) {
-            content()
+            MaterialTheme(shapes = Shapes()) {
+                content()
+            }
         }
     }
+
     @Composable @Preview
-    fun content(){}
+    fun content(){
+        val childWindowsState = groups.associateWith { mutableStateOf(false) }
+        Column {
+            for(group in groups){
+                Button(onClick = { childWindowsState[group]?.value = true }) { Text(group.name) }
+            }
+        }
+        openChildWindows(childWindowsState)
+    }
+
+    @Composable @Preview
+    fun openChildWindows(CWS: Map<Group, MutableState<Boolean>>){
+        //TODO("Открыть окна групп, которые нужно")
+    }
 
 }
--- src/main/kotlin/ru/emkn/kotlin/sms/gui/MainPageController.kt
+++ src/main/kotlin/ru/emkn/kotlin/sms/gui/MainPageController.kt
@@ -6,10 +6,10 @@ import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
-//import androidx.compose.material.
-//GlobalScope
 
 
+//Уж слишком часто нужно
+typealias MSB = MutableState<Boolean>
 
 class MainPageController(val comp: Competitions){
     init{
@@ -29,12 +29,12 @@ class MainPageController(val comp: Competitions){
     Рисует все детали внутри главного окна
      */
     @Composable @Preview fun content(){
-        var gr = remember { mutableStateOf(false) }
-        var ds = remember { mutableStateOf(false) }
-        var tm = remember { mutableStateOf(false) }
-        var sp = remember { mutableStateOf(false) }
-        var cp = remember { mutableStateOf(false) }
-        var pr = remember { mutableStateOf(false) }
+        val gr = remember { mutableStateOf(false) }
+        val ds = remember { mutableStateOf(false) }
+        val tm = remember { mutableStateOf(false) }
+        val sp = remember { mutableStateOf(false) }
+        val cp = remember { mutableStateOf(false) }
+        val pr = remember { mutableStateOf(false) }
 
         Column {
             Button(onClick = { gr.value = true }) { Text("Список групп") }
@@ -44,30 +44,14 @@ class MainPageController(val comp: Competitions){
             Button(onClick = { cp.value = true }) { Text("Все КП") }
             Button(onClick = { pr.value = true }) { Text("Работа с протоколами") }
         }
-        createWindows(gr.value, ds.value, tm.value, sp.value, cp.value, pr.value)
+        createWindows(gr, ds, tm, sp, cp, pr)
     }
 
     /*
     Открывает окна списков групп, команд, ...
      */
-    @Composable @Preview fun createWindows(gr: Boolean, ds: Boolean, tm: Boolean, sp: Boolean, cp: Boolean, pr: Boolean){
-        if(gr)  GroupListController(comp.getGroupsSet().toList()).createWindow()
+    @Composable @Preview fun createWindows(gr: MSB, ds: MSB, tm: MSB, sp: MSB, cp: MSB, pr: MSB){
+        if(gr.value)  GroupListController(comp.getGroupsSet().toList(), gr).createWindow()
 
     }
-    /*
-    //@Composable @Preview
-    private fun openGroupListPage(){
-        //GroupListController().createWindow()
-    }
-
-    fun openTeamListPage(){
-        TeamListController()
-    }
-
-    fun openProtocolPage(){
-        ProtocolPageController()
-    }
-*/
-
-
 }
