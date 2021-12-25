f4d2b2eb42142b61ae4d7feec1b68102c85fbac5
MylnikovNikolay <mylnikovnikolay1707@gmail.com>
2021-12-25 12:34:35 +0300

    Написал вывод дистанции (пока что просто КП в столбец) + начал добавлять везде VerticalScrollbar

--- src/main/kotlin/ru/emkn/kotlin/sms/gui/DistanceController.kt
+++ src/main/kotlin/ru/emkn/kotlin/sms/gui/DistanceController.kt
@@ -1,4 +1,48 @@
 package ru.emkn.kotlin.sms.gui
 
-class DistanceController {
+import androidx.compose.desktop.ui.tooling.preview.Preview
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Shapes
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import ru.emkn.kotlin.sms.*
+import androidx.compose.runtime.MutableState
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.window.Window
+
+class DistanceController(val distance: Distance, val isOpen: MutableState<Boolean>) {
+
+    @Composable
+    @Preview
+    fun createWindow() {
+        Window(
+            onCloseRequest = {isOpen.value=false},
+            title = "Дистанция " + distance.name,
+        ) {
+            MaterialTheme(shapes = Shapes()) {
+                content()
+            }
+        }
+    }
+
+    @Composable
+    @Preview
+    fun content() {
+        val listState = rememberLazyListState()
+        LazyColumn(state = listState) {
+            items(distance.controlPoints) {
+                Text(text = it.name)
+            }
+        }
+
+        VerticalScrollbar(
+            modifier = Modifier.fillMaxHeight(),
+            adapter = rememberScrollbarAdapter(scrollState = listState)
+        )
+    }
 }
--- src/main/kotlin/ru/emkn/kotlin/sms/gui/DistanceListController.kt
+++ src/main/kotlin/ru/emkn/kotlin/sms/gui/DistanceListController.kt
@@ -1,7 +1,12 @@
 package ru.emkn.kotlin.sms.gui
 
 import androidx.compose.desktop.ui.tooling.preview.Preview
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Shapes
@@ -9,6 +14,8 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.window.Window
 import ru.emkn.kotlin.sms.*
 
@@ -29,20 +36,32 @@ class DistanceListController(val distances: List<Distance>, val isOpen: MutableS
 
     @Composable
     @Preview
-    fun content(){
+    fun content() {
         val childWindowsState = distances.associateWith { mutableStateOf(false) }
-        Column {
-            for(dist in distances){
+        val listState = rememberLazyListState()
+
+        LazyColumn(state = listState) {
+            items(distances) { dist ->
                 Button(onClick = { childWindowsState[dist]?.value = true }) { Text(dist.name) }
             }
         }
+
+        VerticalScrollbar(
+            modifier = Modifier.fillMaxHeight(),
+            adapter = rememberScrollbarAdapter(scrollState = listState)
+        )
+
         openChildWindows(childWindowsState)
     }
 
     @Composable
     @Preview
     fun openChildWindows(CWS: Map<Distance, MutableState<Boolean>>){
-        //TODO("Открыть окна дистанций, которые нужно")
+        val toOpen = CWS.filterValues { it.value }.toList()
+        LazyColumn {
+            items(toOpen) { entry ->
+                DistanceController(entry.first, entry.second).createWindow()
+            }
+        }
     }
-
 }
