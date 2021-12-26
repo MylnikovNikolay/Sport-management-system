bf6ae967b97c9d44b747480103aedd818cbfeed3
GrigoriiSolnyshkin <grisolnyshkin04@gmail.com>
2021-12-26 13:11:17 +0300

    СДелал MSL С одной стороны, в нем лежит mutableStateListOf С другой, это наследник обычного листа Через него можно сделать наследника competitions

--- src/test/kotlin/ExperimentingTests.kt
+++ src/test/kotlin/ExperimentingTests.kt
@@ -1,12 +1,10 @@
-import TestCompetitions.Companion.testGeneration
 import ru.emkn.kotlin.sms.*
-import java.io.File
-import kotlin.random.Random
-import kotlin.random.nextInt
 import kotlin.test.*
 
 import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.snapshots.SnapshotStateList
 
 class ExperimentingTests {
     @Test
@@ -16,89 +14,19 @@ class ExperimentingTests {
 
     @Test
     fun test2() {
-        class LMS<T> (val list: List<MutableState<T>>): MutableList<T> {
-            override val size: Int
-                get() = list.size
 
-            override fun contains(element: T): Boolean {
-                return list.contains(mutableStateOf(element))
-            }
+        assertContains(MSL<String>(mutableStateListOf("azaz")), "azaz")
 
-            override fun containsAll(elements: Collection<T>): Boolean {
-                TODO("Not yet implemented")
-            }
+        val msl = MSL<Int>()
+        msl.add(2)
+        msl.add(4)
+        msl.add(5)
+        msl.remove(4)
+        msl.remove(7)
+        assertContains(msl, 2)
+        assertContains(msl, 5)
+        assertEquals(msl.size, 2)
 
-            override fun get(index: Int): T {
-                TODO("Not yet implemented")
-            }
-
-            override fun indexOf(element: T): Int {
-                TODO("Not yet implemented")
-            }
-
-            override fun isEmpty(): Boolean {
-                TODO("Not yet implemented")
-            }
-
-            override fun iterator(): MutableIterator<T> {
-                TODO("Not yet implemented")
-            }
-
-            override fun lastIndexOf(element: T): Int {
-                TODO("Not yet implemented")
-            }
-
-            override fun listIterator(): MutableListIterator<T> {
-                TODO("Not yet implemented")
-            }
-
-            override fun listIterator(index: Int): MutableListIterator<T> {
-                TODO("Not yet implemented")
-            }
-
-            override fun subList(fromIndex: Int, toIndex: Int): MutableList<T> {
-                TODO("Not yet implemented")
-            }
-
-            override fun add(element: T): Boolean {
-                TODO("Not yet implemented")
-            }
-
-            override fun add(index: Int, element: T) {
-                TODO("Not yet implemented")
-            }
-
-            override fun addAll(index: Int, elements: Collection<T>): Boolean {
-                TODO("Not yet implemented")
-            }
-
-            override fun addAll(elements: Collection<T>): Boolean {
-                TODO("Not yet implemented")
-            }
-
-            override fun clear() {
-                TODO("Not yet implemented")
-            }
-
-            override fun remove(element: T): Boolean {
-                TODO("Not yet implemented")
-            }
-
-            override fun removeAll(elements: Collection<T>): Boolean {
-                TODO("Not yet implemented")
-            }
-
-            override fun removeAt(index: Int): T {
-                TODO("Not yet implemented")
-            }
-
-            override fun retainAll(elements: Collection<T>): Boolean {
-                TODO("Not yet implemented")
-            }
-
-            override fun set(index: Int, element: T): T {
-                TODO("Not yet implemented")
-            }
-        }
+        assertEquals(msl.filter {it % 2 == 1}.size, 1)
     }
 }
